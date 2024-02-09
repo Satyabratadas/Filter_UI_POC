c3aac2d2e372189ac344eb01854738ab658e9f93
Satyabrata Das <satyabrata.das@e-arc.com>
2024-02-09 19:44:16 +0530

    screen width change with respect to list width

---
+++ Filter_UI_Dummy_SwiftUI/CheckTwoHandleSlider.swift
@@ -0,0 +1,18 @@
+//
+//  CheckTwoHandleSlider.swift
+//  Filter_UI_Dummy_SwiftUI
+//
+//  Created by Satyabrata Das on 09/02/24.
+//
+
+import SwiftUI
+
+struct CheckTwoHandleSlider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckTwoHandleSlider()
+}
