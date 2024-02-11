a66d71a244c96e6662792c88ed8bf8b1afe1db2e
Satyabrata Das <dassatyabrata557@gmail.com>
2024-02-12 03:27:34 +0530

    slider test update

--- Filter_UI_Dummy_SwiftUI/Filter_UI_Dummy_SwiftUIApp.swift
+++ Filter_UI_Dummy_SwiftUI/Filter_UI_Dummy_SwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Filter_UI_Dummy_SwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SliderTest()
         }
     }
 }
