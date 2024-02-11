aad9f677df361b6a9605097a3a55e6b04f755bad
Satyabrata Das <dassatyabrata557@gmail.com>
2024-02-12 02:39:15 +0530

    sliderTest add

---
+++ SliderTest.swift
@@ -0,0 +1,18 @@
+//
+//  SliderTest.swift
+//  Filter_UI_Dummy_SwiftUI
+//
+//  Created by Satyabrata Das on 12/02/24.
+//
+
+import SwiftUI
+
+struct SliderTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SliderTest()
+}
